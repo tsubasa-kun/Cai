e778b44366e5d4806584b9fb631d2e352ffc8d72
解坤 <xiekun@minivision.cn>
2018-02-05 14:47:34 +0800

    添加WebView页

---
+++ app/src/main/java/com/mario/cai/view/activity/WebActivity.java
@@ -0,0 +1,123 @@
+package com.mario.cai.view.activity;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.love_cookies.cookie_library.activity.BaseActivity;
+import com.love_cookies.cookie_library.utils.NetworkUtils;
+import com.mario.cai.R;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_web)
+public class WebActivity extends BaseActivity {
+
+    @ViewInject(R.id.web_view)
+    private WebView webView;
+
+    /**
+     * 初始化控件
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    public void initWidget(Bundle savedInstanceState) {
+        //声明WebSettings子类
+        WebSettings webSettings = webView.getSettings();
+        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        webSettings.setJavaScriptEnabled(true);
+        //设置自适应屏幕，两者合用
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+        //缩放操作
+        webSettings.setSupportZoom(false); //支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(false); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        //其他细节操作
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        webView.canGoBack();
+    }
+
+    /**
+     * 控件点击事件
+     *
+     * @param view
+     */
+    @Override
+    public void widgetClick(View view) {
+
+    }
+
+    /**
+     * 网络连接了
+     * @param type
+     */
+    @Override
+    public void netConnected(NetworkUtils.NetworkType type) {
+
+    }
+
+    /**
+     * 网络断开了
+     */
+    @Override
+    public void netDisConnected() {
+
+    }
+
+    /**
+     * 点击返回上一页面而不是退出浏览器
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * 销毁WebView
+     */
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            webView.clearHistory();
+            ((ViewGroup) webView.getParent()).removeView(webView);
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
+    }
+}
