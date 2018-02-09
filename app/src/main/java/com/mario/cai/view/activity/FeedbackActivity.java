6825060de5f587bc2377155255bed84c0ea63e23
解坤 <xiekun@minivision.cn>
2018-02-09 15:16:53 +0800

    完善各个页面

--- app/src/main/java/com/mario/cai/view/activity/FeedbackActivity.java
+++ app/src/main/java/com/mario/cai/view/activity/FeedbackActivity.java
@@ -2,9 +2,12 @@ package com.mario.cai.view.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.love_cookies.cookie_library.activity.BaseActivity;
 import com.love_cookies.cookie_library.utils.NetworkUtils;
+import com.love_cookies.cookie_library.utils.ToastUtils;
 import com.love_cookies.cookie_library.widget.CookieTitleBar;
 import com.mario.cai.R;
 
@@ -21,6 +24,15 @@ public class FeedbackActivity extends BaseActivity {
 
     @ViewInject(R.id.title_bar)
     private CookieTitleBar titleBar;
+    @ViewInject(R.id.feedback)
+    private EditText feedbackEt;
+    @ViewInject(R.id.contact)
+    private EditText contactEt;
+    @ViewInject(R.id.submit_btn)
+    private Button submitBtn;
+
+    private String feedback;
+    private String contact;
 
     /**
      * 初始化控件
@@ -36,6 +48,7 @@ public class FeedbackActivity extends BaseActivity {
                 finish();
             }
         });
+        submitBtn.setOnClickListener(this);
     }
 
     /**
@@ -44,7 +57,9 @@ public class FeedbackActivity extends BaseActivity {
      */
     @Override
     public void widgetClick(View view) {
-
+        if (dataIsValid()) {
+            //TODO 提交
+        }
     }
 
     /**
@@ -62,5 +77,23 @@ public class FeedbackActivity extends BaseActivity {
     @Override
     public void netDisConnected() {
 
+    }
+
+    /**
+     * 校验数据合法性
+     * @return
+     */
+    public boolean dataIsValid() {
+        boolean valid = false;
+        feedback = feedbackEt.getText().toString().trim();
+        contact = contactEt.getText().toString().trim();
+        if (feedback == null || feedback.length() <= 0) {
+            ToastUtils.show(this, R.string.feedback_tip);
+        } else if (contact == null || contact.length() <= 0) {
+            ToastUtils.show(this, R.string.contact_tip);
+        } else {
+            valid = true;
+        }
+        return valid;
     }
 }
